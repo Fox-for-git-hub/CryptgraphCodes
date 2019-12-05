71aa1f6934aeb0fcf75d5170cd50499df89c15ea
工藤千寛 <kudousenhiroshi@ChihiroKudo.local>
2019-12-05 18:24:40 +0900

    Added code of create prime number and its list

--- SimpleRSA.py
+++ SimpleRSA.py
@@ -1,8 +1,10 @@
 import random
 import math
+from sympy import prime
+
 
 #暗号化する平文
-message = 777
+message = 19961031
 
 
 ########################
@@ -10,8 +12,14 @@ message = 777
 ########################
 
 #素数配列の作成
-p_candidates = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149]
-q_candidates = [151, 157, 163, 167, 173, 179, 181, 191, 193, 197]
+for i in range(500, 600):
+    p_candidates = []
+    p_candidates.append(prime(i))
+    print (p_candidates)
+
+    q_candidates = []
+    q_candidates.append(prime(i+100))
+    print (q_candidates)
 
 #randomモジュールの関数でランダムに要素を1つ選択
 p = random.choice(p_candidates)
@@ -37,16 +45,18 @@ E = random.randrange(2, L)
 while math.gcd(E, L) != 1:
     #再度乱数を発生
     E = random.randrange(2, L)
+    print (E)
 
 
 ########################
 ###### 秘密鍵の作成 ######
 ########################
 
-for i in range (2, L):
+for i in range(2, L):
     if (E * i) % L == 1:
         D = i
         break
+    print(E)
 
 
 ########################
@@ -65,3 +75,16 @@ print ('crypted message is : ' + str(cipher) + '\n')
 decipher = pow(cipher, D) % N
 
 print ('decrypted message is : ' + str(decipher) + '\n')
+
+
+
+#########################
+###### 総当たり攻撃 ######
+#########################
+
+
+#for (j in range(?, ?)):
+ #   predecipher = pow(cipher, ?) % ?
+  #  print ('predecrypted message is :' + str(predecipher) + '\n')
+   # if (predecipher == message):
+    #    break
